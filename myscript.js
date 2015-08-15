45c045c582ac45316f212624f9130f32d9de7e05
Ernie Ho <g41903@gmail.com>
2015-08-15 15:58:54 +0800

    First Commit

---
+++ myscript.js
@@ -0,0 +1,5 @@
+// document.body.innerHTML="NO MORE FACEBOOK!!!!!!!!";
+// document.body.style.color="red";
+// document.body.style.fontSize="100pt";
+// document.body.style.textAlign="center";
+ chrome.extension.sendRequest({redirect: "http://www.economist.com/"}); // send message to redirect
